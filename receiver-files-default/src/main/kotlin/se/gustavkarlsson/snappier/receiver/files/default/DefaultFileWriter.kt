0ae54925573f70d6bb32e6db4b13444df8b5a7f4
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-05-26 21:49:14 +0200

    Simplify DefaultFileWriter

--- receiver-files-default/src/main/kotlin/se/gustavkarlsson/snappier/receiver/files/default/DefaultFileWriter.kt
+++ receiver-files-default/src/main/kotlin/se/gustavkarlsson/snappier/receiver/files/default/DefaultFileWriter.kt
@@ -15,32 +15,29 @@ class DefaultFileWriter(
     private var currentStream: BufferedOutputStream? = null
 
     override fun create(path: String): Completable =
-        Completable.fromAction {
-            synchronized(this) {
-                check(currentStream == null) { "Stream is not null: $currentStream" }
-                val file = File(path)
-                file.parentFile.mkdirs()
-                val created = file.createNewFile()
-                check(created) { "File already exists: $file" }
-                currentStream = file.outputStream().buffered(writeBufferSize)
-            }
-        }.subscribeOn(scheduler)
+        completable {
+            check(currentStream == null) { "Stream is not null: $currentStream" }
+            val file = File(path)
+            file.parentFile.mkdirs()
+            val created = file.createNewFile()
+            check(created) { "File already exists: $file" }
+            currentStream = file.outputStream().buffered(writeBufferSize)
+        }
 
     override fun write(data: ByteArray): Completable =
-        Completable.fromAction {
-            synchronized(this) {
-                val stream = checkNotNull(currentStream) { "Stream is null" }
-                stream.write(data)
-            }
-        }.subscribeOn(scheduler)
+        completable {
+            val stream = checkNotNull(currentStream) { "Stream is null" }
+            stream.write(data)
+        }
 
     override fun close(): Completable =
-        Completable.fromAction {
-            synchronized(this) {
-                val stream = checkNotNull(currentStream) { "Stream is null" }
-                stream.flush()
-                stream.close()
-                currentStream = null
-            }
-        }.subscribeOn(scheduler)
+        completable {
+            val stream = checkNotNull(currentStream) { "Stream is null" }
+            stream.flush()
+            stream.close()
+            currentStream = null
+        }
+
+    private fun completable(block: () -> Unit) =
+        Completable.fromAction(block).subscribeOn(scheduler)
 }
